ec6d61afc0d31a0c2dede3a0f7e85d9861436164
chillchillchou <hc2446@nyu.edu>
2019-03-29 19:47:04 -0400

    fix indentation

--- master_picam.py
+++ master_picam.py
@@ -100,18 +100,18 @@ def pressButton():
         response = rekognition.detect_faces(
             	  Image={'Bytes': image.read()}, Attributes=['ALL'])
     	# pprint (response)
-    	print('Detected faces for ' + name)
-    	os.system("espeak Hello," + name)
-    	no_emotion = True
-    	for faceDetail in response['FaceDetails']:
-        		for emotion in faceDetail['Emotions']:
-            		if emotion['Confidence'] > 50:
-                        emotion_str = str(emotion['Type'])
-                		print("looks like you are," + emotion_str)
-                		os.system("espeak \'Looks like you are\'"+emotion_str);
-             			no_emotion=False
-        if no_emotion:
-            os.system("espeak 'I can not tell your emotion'")
+    print('Detected faces for ' + name)
+    os.system("espeak Hello," + name)
+    no_emotion = True
+    for faceDetail in response['FaceDetails']:
+        	for emotion in faceDetail['Emotions']:
+            	if emotion['Confidence'] > 50:
+                    emotion_str = str(emotion['Type'])
+                	print("looks like you are," + emotion_str)
+                	os.system("espeak \'Looks like you are\'"+emotion_str);
+             		no_emotion=False
+    if no_emotion:
+        os.system("espeak 'I can not tell your emotion'")
 
 button.when_pressed = pressButton()
 pause()
