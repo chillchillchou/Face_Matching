e9be091b24bccbed3c0f3e4967307f041c1675b5
chillchillchou <huiyiche@usc.edu>
2019-04-24 23:43:26 -0400

    add push button

--- led.py
+++ led.py
@@ -1,8 +1,13 @@
 from gpiozero import LED
 from time import sleep
-led = LED(23)
-while True:
-    led.on
-    sleep(5)
-    led.off()
-    sleep(1)
+
+led1 = LED(23)
+led2 = LED(24)
+button = Button(25)
+buttom.wati_for_press()
+print("button pressed")
+led1.on()
+led2.on()
+sleep(3)
+led1.off()
+led2.off()
