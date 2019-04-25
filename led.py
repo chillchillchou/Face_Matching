b8b591c17ff0f0f929debb359dbfb250d423173b
chillchillchou <huiyiche@usc.edu>
2019-04-24 23:53:17 -0400

    edit led

--- led.py
+++ led.py
@@ -1,14 +1,14 @@
 from gpiozero import LED, Button
 from time import sleep
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-button=Button(25)
-button.wait_for_press()
+
+led1 = LED(23)
+led2 = LED(24)
+button = Button(25)
+buttom.wait_for_press()
 print("button pressed")
-GPIO.output(23, GPIO.HIGH) # Turn on
-sleep(1) # Sleep for 1 second
-GPIO.output(23, GPIO.LOW) # Turn off
-sleep(1) # Sleep for 1 second
+led1.on()
+led2.on()
+sleep(1)
+led1.off()
+led2.off()
+sleep(1)
