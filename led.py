e1f6f4be306fbab87848b2b7cf2aa40a06e41822
chillchillchou <huiyiche@usc.edu>
2019-04-24 23:51:08 -0400

    edit led

--- led.py
+++ led.py
@@ -5,8 +5,10 @@ from time import sleep # Import the sleep function from the time module
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-while True:
-    GPIO.output(23, GPIO.HIGH) # Turn on
-    sleep(1) # Sleep for 1 second
-    GPIO.output(23, GPIO.LOW) # Turn off
-    sleep(1) # Sleep for 1 second
+button=Button(25)
+button.wati_for_press()
+print("button pressed")
+GPIO.output(23, GPIO.HIGH) # Turn on
+sleep(1) # Sleep for 1 second
+GPIO.output(23, GPIO.LOW) # Turn off
+sleep(1) # Sleep for 1 second
