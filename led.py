8c00d07df31da28bb3c84813bc85e2739d615516
chillchillchou <huiyiche@usc.edu>
2019-04-24 23:51:41 -0400

    edit led

--- led.py
+++ led.py
@@ -6,7 +6,7 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 button=Button(25)
-button.wati_for_press()
+button.wait_for_press()
 print("button pressed")
 GPIO.output(23, GPIO.HIGH) # Turn on
 sleep(1) # Sleep for 1 second
