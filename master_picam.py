2211babcf39808c6fb9f998a9c01cbd7e1407576
chillchillchou <huiyiche@usc.edu>
2019-04-27 14:54:11 -0400

    button

--- master_picam.py
+++ master_picam.py
@@ -195,6 +195,7 @@ def main():
 
         while True:  # comment this out if you
             if button1.is_held:
+                print("buton 1 held.")
                 if not button_1_press_triggered:
                     print('triggering button 1')
                     button_1_pressed(camera, stream)
